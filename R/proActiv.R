fd8be60b471199a38ee92229c94547db3b442967
jleechung <joseph.lee@u.nus.edu>
2020-06-12 15:30:21 +0800

    simplify argument names, allow compressed files and duplicate names

--- R/proActiv.R
+++ R/proActiv.R
@@ -1,17 +1,17 @@
 #' Wrapper function returning Summarized Experiment object giving promoter counts and activity
 #'
-#' @param promoterAnnotationData A PromoterAnnotation object containing the
+#' @param files A character vector. The list of input files for 
+#'   which the junction read counts will be calculated
+#' @param promoterAnnotation A PromoterAnnotation object containing the
 #'   reduced exon ranges, annotated intron ranges, promoter coordinates and the
 #'   promoter id mapping
-#' @param filePaths A character vector. The list of input files for 
-#'   which the junction read counts will be calculated
 #' @param fileLabels A character vector. The labels of input files 
 #'   for which the junction read counts will be calculated. These labels will be 
 #'   used as column names for each output data.frame object. If not provided,
 #'   filenames will be used as labels. Defaults to NULL
 #' @param genome A character. Genome version. Must be specified if input file
 #'   type is a BAM file. Defaults to NULL
-#' @param numberOfCores A numeric value. The number of cores to be used for 
+#' @param ncores A numeric value. The number of cores to be used for 
 #'   counting junction reads. Defaults to 1 (no parallelization). This parameter 
 #'   will be used as an argument to BiocParallel::bplapply
 #' 
@@ -23,40 +23,44 @@
 #' 
 #' @examples
 #' \dontrun{
-#' filePaths <- c('./sample1-tophat.bed', './sample2-tophat.bed')
-#' proActivFromJunctions <- proActiv(promoterAnnotationData = promoterAnnotationData, 
-#'                                  filePaths = filePaths, 
+#' files <- c('./sample1-tophat.bed', './sample2-tophat.bed')
+#' proActivFromJunctions <- proActiv(files = files,
+#'                                   promoterAnnotation = promoterAnnotation, 
 #'                                  fileLabels = NULL, 
 #'                                  genome = NULL, 
-#'                                  numberOfCores = 1)
+#'                                  ncores = 1)
 #' 
-#' filePaths <- c('./sample1.bam', './sample2.bam')
-#' proActivFromBAM <- proActiv(promoterAnnotation Data = promoterAnnotationData,
-#'                            filePaths = filePaths,
+#' files <- c('./sample1.bam', './sample2.bam')
+#' proActivFromBAM <- proActiv(files = files,
+#'                             promoterAnnotation  = promoterAnnotation,
 #'                            fileLabels = NULL,
 #'                            genome = 'hg19',
-#'                            numberOfCores = 1)}
+#'                            ncores = 1)}
 #'                            
 #' @import SummarizedExperiment
 #' @import S4Vectors
 #'
-proActiv <- function(promoterAnnotationData, filePaths = NULL, fileLabels = NULL, 
-                     genome = NULL, numberOfCores = 1) {
+proActiv <- function(files = NULL, promoterAnnotation, fileLabels = NULL, 
+                     genome = NULL, ncores = 1) {
   
-  if (is.null(filePaths)) {
+  if (is.null(files)) {
     stop(paste0('Error: Please specify valid file paths!'))
   }
   
-  checkFile <- file.exists(filePaths)
+  checkFile <- file.exists(files)
   if (any(!checkFile)) {
-    stop(paste0('Error: Please specify valid file paths. The following file does not exist: ', filePaths[!checkFile]))
+    stop(paste0('Error: Please specify valid file paths. The following file does not exist: ', files[!checkFile]))
   }
   
   if (is.null(fileLabels)) {
-    fileLabels <- tools::file_path_sans_ext(basename(filePaths))
+    fileLabels <- make.names(tools::file_path_sans_ext(basename(files), compression = TRUE), unique = TRUE)
   }
   
-  ext <- unique(tools::file_ext(filePaths))
+  ext <- unique(tools::file_ext(files))
+  if (ext == 'gz' | ext == 'bz2' | ext == 'xz'){
+    files.tmp <- gsub(paste0('\\.', ext), '', files)
+    ext <- unique(tools::file_ext(files.tmp))
+  }
   
   if (length(ext) != 1) {
     stop("Error: More than one file type detected from given file path")
@@ -71,10 +75,10 @@ proActiv <- function(promoterAnnotationData, filePaths = NULL, fileLabels = NULL
        Tophat junctions file (.bed) or STAR junctions file (.junctions)')
   }
   
-  promoterCounts <- calculatePromoterReadCounts(promoterAnnotationData, filePaths, fileLabels, 
-                                                fileType, genome, numberOfCores)
+  promoterCounts <- calculatePromoterReadCounts(promoterAnnotation, files, fileLabels, 
+                                                fileType, genome, ncores)
   normalizedPromoterCounts <- normalizePromoterReadCounts(promoterCounts)
-  absolutePromoterActivity <- getAbsolutePromoterActivity(normalizedPromoterCounts, promoterAnnotationData)
+  absolutePromoterActivity <- getAbsolutePromoterActivity(normalizedPromoterCounts, promoterAnnotation)
   geneExpression <- getGeneExpression(absolutePromoterActivity)
   relativePromoterActivity <- getRelativePromoterActivity(absolutePromoterActivity, geneExpression)
   
