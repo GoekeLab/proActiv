477fe2f82eb4db4fa960eb0eb9ca43bfd1d23a3c
Joseph Lee <joseph.lee@u.nus.edu>
2020-06-29 22:05:41 +0800

    update tests for annotation code

--- tests/testthat/test_annotation-data.R
+++ tests/testthat/test_annotation-data.R
@@ -2,42 +2,48 @@ context('Calculating Promoter Annotation')
 library(proActiv)
 library(mockery)
 
-gtf <- list.files(system.file('/extdata/testdata/promoterAnnotation', package = 'proActiv'), full.names = TRUE, pattern = 'gtf')
-txdb <- list.files(system.file('/extdata/testdata/promoterAnnotation', package = 'proActiv'), full.names = TRUE, pattern = 'sqlite')
+gtfPath <- list.files(system.file('/extdata/testdata/promoterAnnotation', package = 'proActiv'), full.names = TRUE, pattern = 'gtf')
+txdbPath <- list.files(system.file('/extdata/testdata/promoterAnnotation', package = 'proActiv'), full.names = TRUE, pattern = 'sqlite')
+
+txdb <- AnnotationDbi::loadDb(txdbPath)
 
 reducedExonRanges <- readRDS(system.file('/extdata/testdata/promoterAnnotation', 'reducedExonRanges.rds', package = 'proActiv'))
 annotatedIntronRanges <- readRDS(system.file('/extdata/testdata/promoterAnnotation', 'annotatedIntronRanges.rds', package = 'proActiv'))
 promoterIdMapping <- readRDS(system.file('/extdata/testdata/promoterAnnotation', 'promoterIdMapping.rds', package = 'proActiv'))
 promoterCoordinates <- readRDS(system.file('/extdata/testdata/promoterAnnotation', 'promoterCoordinates.rds', package = 'proActiv'))
 
-test_that('proActiv handles multiple file types', {
+test_that('preparePromoterAnnotation handles ambiguous argument specification', {
+  expect_error(preparePromoterAnnotation(gtfPath, 'Homo_sapiens'))
+})
+
+test_that('preparePromoterAnnotation handles multiple file types', {
   
-  mockery::stub(proActiv, 'file.exists', function() TRUE)
-  files <- c('sample1.bed', 'sample2.junctions')
-  expect_error(proActiv(files, promoterAnnotation))
+  mockery::stub(preparePromoterAnnotation, 'file.exists', function() TRUE)
+  files <- 'sample.gtf'
+  expect_error(preparePromoterAnnotation(file = file, species = 'Homo_sapiens'))
   
 })
 
 test_that('preparePromoterAnnotation handles non-existent files', {
   
-  file <- 'tmp.txt'
-  expect_error(preparePromoterAnnotation(file, 'Homo_sapiens'))
+  file <- 'sample.txt'
+  expect_error(preparePromoterAnnotation(file = file, species = 'Homo_sapiens'))
   
 })
 
 test_that('preparePromoterAnnotation handles invalid file types', {
   
-  mockery::stub(proActiv, 'file.exists', function() TRUE)
-  file <- c('tmp.txt')
-  expect_error(preparePromoterAnnotation(file, 'Homo_sapiens'))
+  mockery::stub(preparePromoterAnnotation, 'file.exists', function() TRUE)
+  file <- 'sample.txt'
+  expect_error(preparePromoterAnnotation(file = file, species = 'Homo_sapiens'))
   
 })
 
 
-test_that('preparePromoterAnnotation returns expected output with gtf',{
+test_that('preparePromoterAnnotation returns expected output with gtf file path',{
   
   suppressWarnings(
-    promoterAnnotation <- preparePromoterAnnotation(gtf, 'Homo_sapiens')
+    promoterAnnotation <- preparePromoterAnnotation(file = gtfPath, species = 'Homo_sapiens')
   )
   expect_s4_class(intronRanges(promoterAnnotation), 'GRanges')
   expect_type(promoterIdMapping(promoterAnnotation), 'list')
@@ -50,10 +56,26 @@ test_that('preparePromoterAnnotation returns expected output with gtf',{
   
 })
 
-test_that('preparePromoterAnnotation returns expected output with txdb',{
+test_that('preparePromoterAnnotation returns expected output with txdb file path',{
   
   suppressWarnings(
-    promoterAnnotation <- preparePromoterAnnotation(txdb, 'Homo_sapiens')
+    promoterAnnotation <- preparePromoterAnnotation(file = txdbPath, species = 'Homo_sapiens')
+  )
+  expect_s4_class(intronRanges(promoterAnnotation), 'GRanges')
+  expect_type(promoterIdMapping(promoterAnnotation), 'list')
+  expect_s4_class(promoterCoordinates(promoterAnnotation), 'GRanges')
+  
+  expect_equal(intronRanges(promoterAnnotation), annotatedIntronRanges[,c('INTRONID', 'TXNAME')])
+  expect_equal(promoterIdMapping(promoterAnnotation), promoterIdMapping[,c('transcriptName', 'promoterId', 'geneId')])
+  expect_equal(promoterCoordinates(promoterAnnotation)[,c('promoterId', 'geneId', 'internalPromoter')], promoterCoordinates)
+  expect_equal(mcols(promoterCoordinates(promoterAnnotation))$intronId, mcols(reducedExonRanges)$intronId)
+  
+})
+
+test_that('preparePromoterAnnotation returns expected output with txdb object', {
+  
+  suppressWarnings(
+    promoterAnnotation <- preparePromoterAnnotation(txdb = txdb, species = 'Homo_sapiens')
   )
   expect_s4_class(intronRanges(promoterAnnotation), 'GRanges')
   expect_type(promoterIdMapping(promoterAnnotation), 'list')
